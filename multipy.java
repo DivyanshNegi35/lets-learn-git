6f436445d91d18fdf56350c64edf46b6d69e87dc
Divyansh Negi <devashishlpu111@gmail.com>
2021-12-01 17:42:49 +0530

    add multipy.java

---
+++ multipy.java
@@ -0,0 +1,12 @@
+public class Sum {
+                public static void main(Strings[] args){
+                        int a=5;
+                        int b=10;
+                        int c=90
+                        System.out.println(a * b * c);
+
+                }
+
+}
+
+

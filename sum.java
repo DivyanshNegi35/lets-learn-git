b0c6185a4c81d142bb1f576ce816f3fa11e0b84f
Divyansh Negi <devashishlpu111@gmail.com>
2021-12-02 12:12:33 +0530

    add one vairable for multiply sum.java

--- sum.java
+++ sum.java
@@ -2,8 +2,9 @@ public class Sum {
 		public static void main(Strings[] args){
 			int a=5;
 			int b=10;
-                        int c=90
-			System.out.println(a + b + c);
+                        int c=90;
+			int d=90;
+			System.out.println(a + b + c *(D));
 
 		}
 
